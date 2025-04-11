8f0c06084238b3bd801d1c394e2b1d616952bbe6
martinromario55 <martinromario55@gmail.com>
2025-04-11 21:52:31 +0300

    Expense Tracker - Home Screen

---
+++ app/src/main/java/com/tuyiiya/expensetracker/HomeScreeen.kt
@@ -0,0 +1,288 @@
+package com.tuyiiya.expensetracker
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.tuyiiya.expensetracker.ui.theme.ExpenseTrackerTheme
+import com.tuyiiya.expensetracker.ui.theme.Zinc
+
+@Composable
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val (nameRow, list, card, topBar) = createRefs()
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_topbar),
+                contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 32.dp,
+                        start = 16.dp,
+                        end = 16.dp
+                    )
+                    .constrainAs(nameRow) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text(
+                        text = "Good Afternoon",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+
+                    Text(
+                        text = "Tuyiiya",
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+
+            CardItem(
+                modifier = Modifier.constrainAs(card) {
+                    top.linkTo(nameRow.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
+            TransactionList(modifier = Modifier.fillMaxWidth().constrainAs(list) {
+                top.linkTo(card.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+                height = Dimension.fillToConstraints
+            })
+        }
+    }
+}
+
+
+@Composable
+fun CardItem(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .height(200.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Zinc)
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    Column {
+                        Text(text = "Total Balance", fontSize = 16.sp, color = Color.White)
+                        Text(
+                            text = "UGX 1,000,000",
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+
+                    Image(
+                        painter = painterResource(id = R.drawable.dots_menu),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        CardRowItem(
+                            title = "Income",
+                            amount = "700,000",
+                            image = R.drawable.ic_income
+                        )
+
+                        CardRowItem(
+                            title = "Expense",
+                            amount = "430,000",
+                            image = R.drawable.ic_expense
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TransactionList(modifier: Modifier) {
+    Column(modifier = modifier.padding(horizontal = 16.dp)) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Recent Transactions", fontSize = 20.sp)
+            Text(
+                text = "See All",
+                fontSize = 16.sp,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+
+        TransactionItem(
+            title = "Netflix",
+            amount = "200.00",
+            icon = R.drawable.ic_netflix,
+            date = "Today",
+            color = Color.Red,
+        )
+        TransactionItem(
+            title = "Upwork",
+            amount = "200.00",
+            icon = R.drawable.ic_upwork,
+            date = "Today",
+            color = Color.Green,
+        )
+        TransactionItem(
+            title = "Paypal",
+            amount = "200.00",
+            icon = R.drawable.ic_paypal,
+            date = "Today",
+            color = Color.Green,
+        )
+        TransactionItem(
+            title = "Starbucks",
+            amount = "200.00",
+            icon = R.drawable.ic_starbucks,
+            date = "Today",
+            color = Color.Red,
+        )
+    }
+}
+
+
+@Composable
+fun TransactionItem(
+    title: String,
+    amount: String,
+    icon: Int,
+    date: String,
+    color: Color
+) {
+    Box(modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Column {
+                Text(text = title, fontSize = 16.sp)
+                Text(text = date, fontSize = 17.sp, color = Color.Gray)
+            }
+        }
+        Text(
+            text = "UGX $amount",
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterEnd),
+            color = color
+        )
+    }
+}
+
+
+@Composable
+fun CardRowItem(
+    title: String,
+    amount: String,
+    image: Int
+) {
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = title, fontSize = 16.sp, color = Color.White)
+        }
+        Text(text = "UGX $amount", fontSize = 20.sp, color = Color.White)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
---
+++ app/src/main/java/com/tuyiiya/expensetracker/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.tuyiiya.expensetracker
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tuyiiya.expensetracker.ui.theme.ExpenseTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExpenseTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
